02f8480819791069be8da2c91d46836f74affff8
K_XPS15 <jizj@icloud.com>
2023-03-30 21:14:15 +0800

    加入了打击之后飞走

--- src/server/s-global-init.server.lua
+++ src/server/s-global-init.server.lua
@@ -3,7 +3,7 @@ Players = game:GetService("Players")
 RunService.Stepped:Connect(function(_time,delta)
     for i,player in pairs(Players:GetPlayers()) do
     if player.Character and player.Character.Model then
-        local newCF = CFrame.New(player.Character:GetPivot().Position) 
+        local newCF = CFrame.new(player.Character:GetPivot().Position) 
         local rot = player.Character.Model:GetPivot().Rotation * CFrame.Angles(0,0.1,0)
         player.Character.Model:PivotTo(newCF*rot)
     end
