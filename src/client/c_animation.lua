862b5ff3836aa3e140adb292c1aa720a2423465d
K_MINI_PC <jizj@icloud.com>
2023-04-23 00:07:52 +0800

    可能修改了死后动画有问题的bug

--- src/client/c_animation.lua
+++ src/client/c_animation.lua
@@ -1,17 +1,26 @@
 print("first require: ", script.Name)
 local module = {}
-local rotAnimation = "rbxassetid://13019768278"
+-- local rotAnimation = "rbxassetid://13019768278"
+local rotAnimation = "rbxassetid://13201628620"
 
-local character = game:GetService("Players").LocalPlayer.Character
-local humanoid = character:WaitForChild("Humanoid") :: Humanoid
-local animator = humanoid:WaitForChild("Animator")
-local animation = Instance.new("Animation",workspace)
-animation.Name = "selfAnimation"
-animation.AnimationId = rotAnimation
-local animationTrack = animator:LoadAnimation(animation)
+local animationTrack
+local function preLoadAni()
+    local character = game:GetService("Players").LocalPlayer.Character
+    local humanoid = character:WaitForChild("Humanoid") :: Humanoid
+    local animator = humanoid:WaitForChild("Animator")
+    local animation = Instance.new("Animation",workspace)
+    animation.Name = "selfAnimation"
+    animation.AnimationId = rotAnimation
+    animationTrack = animator:LoadAnimation(animation)
+end
+
+preLoadAni()
 
 
 function module.onlyRot(player:Player)
+    print("module.onlyRot")
+    local character = game:GetService("Players").LocalPlayer.Character
+    local humanoid = character:WaitForChild("Humanoid") :: Humanoid
     local animateScript = character:WaitForChild("Animate")
     animateScript.Enabled = false
     local animator = humanoid:WaitForChild("Animator")
@@ -28,13 +37,12 @@ function module.onlyRot(player:Player)
     animationTrack:Play(0,10)
 end
 
-
-
 function module.resetAnim(player:Player)
     animationTrack:Stop(0)
     local character = player.Character
     local animateScript = character:WaitForChild("Animate")
     animateScript.Enabled = true
+    preLoadAni()
 end
 
 return module
